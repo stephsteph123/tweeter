fdb01ea2f005803bd235c3dd7561dac90105ee92
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-12-10 14:45:00 -0500

    updates to the GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,55 +4,79 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+  const createTweetElement = function (tweet) {
+    return `<article class="tweetContainer">
+  <h2 class="articleTweetHeader">
+    <img class="userIcon" src=${tweet.user.avatars}>
+    <tag class ="userName">${tweet.user.name}</tag>
+    <tag class ="userHandle">${tweet.user.handle}</tag>
+  </h2>
+  
+  <tag class ="tweetBody">${tweet.content.text}</tag>
+  <tag class ="borderLine">borderline </tag>
+  <footer class="articleTweetFooter">
+    <time class="tweetDate">${tweet.created_at}</time>
+    <span class="rightLogos">
+      <i class="fa-solid fa-flag" id="flagIMG"></i>
+      <i class="fa-solid fa-recycle" id="repostIMG"></i>
+      <i class="fa-solid fa-heart" id="heartIMG"></i>
+    </span>
+  </footer>
+  </article>`
+  }
+
+  const renderTweets = function (tweets) {
+    $("tweetContainer").empty();
+    for (let tweet of tweets) {
+      $("#tweetContainer").prepend(createTweetElement(tweet))
+    }
+  }
+
+  const loadTweets = function () {
+    $.ajax({  url: "http://localhost:8080/tweets", method: "GET" })
+    .then(function(tweetsBody) {
+      console.log("success", tweetsBody)
+      renderTweets(tweetsBody)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    }
+
 $(document).ready(function(){
-const createTweetElement = function (tweet) {
-  return `<article class="tweetContainer">
-<h2 class="articleTweetHeader">
-  <img class="userIcon" src=${tweet.user.avatars}>
-  <tag class ="userName">${tweet.user.name}</tag>
-  <tag class ="userHandle">${tweet.user.handle}</tag>
-</h2>
 
-<tag class ="tweetBody">${tweet.content.text}</tag>
-<tag class ="borderLine">borderline </tag>
-<footer class="articleTweetFooter">
-  <time class="tweetDate">${tweet.created_at}</time>
-  <span class="rightLogos">
-    <i class="fa-solid fa-flag" id="flagIMG"></i>
-    <i class="fa-solid fa-recycle" id="repostIMG"></i>
-    <i class="fa-solid fa-heart" id="heartIMG"></i>
-  </span>
-</footer>
-</article>`
-}
+loadTweets();
 
-const renderTweets = function (tweets) {
-  for (let tweet of tweets) {
-    $("#tweetContainer").prepend(createTweetElement(tweet))
+$(".formContainer").submit(function(event) {
+  event.preventDefault();
+  const missText = $("#tweet-text").val();
+  if (!missText) {
+    alert("tweet cannot be empty");
+  } else {
+    $.ajax({  
+      url: "/tweets", 
+      method: "POST", 
+      data: $(".formContainer").serialize(),
+    })
+    .then(function(tweetsBody) {
+      console.log("success", tweetsBody)
+      loadTweets();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
-}
-
-// hardcode example:
-
-const tweetData = [
-  {
-    "user": {
-      "name": "bye",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1670443757712
-  }
-]
-
-renderTweets(tweetData);
 })
+
+});
+
+
+
 
 //using AJAX to fetch from server
 //inside (docoument.ready), create funciton > laodtweets
 //fetch tweets from tweets page.
-//JSON inside HTML
-
+//JSON inside HTML 
+//pass to renderTweets function (AJAX GET)
+//implement timego
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,17 @@
-// $('.formContainer').submit((event) => {
-//   event.preventDefault();
-//   $("#tweet-text").keyup(); 
-//   myFunction();
-// });
-
-// const myFunction = function() {
-//   console.log("let maxLength= 120");
-// }
-//   // .keyup(function() {
-//   //   let length = val().length;
-//   //   length = maxLength-length;
-//   // })
-
 
 $("#tweet-text").keyup(function() {
   const maxLength = 140;
   const inputCount = $("#tweet-text").val().length;
   let newCount = maxLength-inputCount
-  console.log(newCount)
+  if (newCount> 130) {
+    //text turns red
+  }
   $('.counterCount').text(newCount);
+});
+
+$(".buttonSubmit").click(function() {
+  const missText = $("#tweet-text").val();
+  if (!missText) {
+    alert("tweet cannot be empty");
+  }
 });
